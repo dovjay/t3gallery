a7dc559a54abf472f1f6c8d71196adc1e73b7d9d
Dovan Sanjaya <dovansanjaya24@gmail.com>
2024-05-20 14:30:58 +0700

    actually linking the database to UI

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 - [x] Scaffold basic ui with mock data
 - [x] Tidy up build process
 - [x] Actually set up a database (vercel postgres)
+- [x] Attach database to UI
 - [ ] Add authentication (w/ clerk)
 - [ ] Add image upload
 - [ ] Error management (w/ Sentry)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,20 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://plus.unsplash.com/premium_photo-1708110921152-850148b86156?q=80&w=1960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1715588561967-6d413b6690bf?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1715893743617-056b988f1e73?q=80&w=1941&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1716034351843-98080eafa00c?q=80&w=1938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}))
-
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map(post => (
-          <div key={post.id}>{post.name}</div>
-        ))}
         {
-          [...mockImages, ...mockImages, ...mockImages].map((image, i) => (
-            <div key={image.id + "-" + i} className="w-48">
+          [...images, ...images, ...images].map((image, i) => (
+            <div key={image.id + "-" + i} className="w-48 flex flex-col">
               <img src={image.url} />
+              <div>{image.name}</div>
             </div>
           ))
         }
